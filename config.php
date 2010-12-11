351ee1fb5c73fcef4d3ea4bfac0fc74cf9ce675f
Pieter Colpaert <pieter@irail.be>
2010-12-11 14:48:49 +0100

    user agent updated

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 global $APIurl;
 $APIurl = "http://api.irail.be/";
 global $iRailAgent;
-$iRailAgent = "Mobile website 1.5";
+$iRailAgent = "Mobile website 1.5 - " . $_SERVER['HTTP_USER_AGENT'];
 
 
 ?>
