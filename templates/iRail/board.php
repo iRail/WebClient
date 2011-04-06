513ccf300d03b961be9724818fbb57fcd254c432
Yeri Tiete <yeri@tiete.be>
2011-04-06 11:22:38 +0200

    hidden url bar test

--- templates/iRail/board.php
+++ templates/iRail/board.php
@@ -11,6 +11,7 @@ if(isset($_GET['from'])){
 <!DOCTYPE html>
 <html lang="en" appcache="/appcache.mf">    
     <head>
+	<meta name="apple-mobile-web-app-capable"  content="yes" />
         <meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.6, user-scalable=no" />
         <meta name="keywords" content="nmbs, sncb, iphone, mobile, irail, irail.be, route planner"/>
