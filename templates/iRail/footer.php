2438e139abf5fa775a9054da1fc9760b4ba74423
Mathias Bynens <mathias@qiwi.be>
2011-06-08 11:24:19 -0700

    Remove `.async = true` from GA snippet as it’s not needed in this particular case. See http://mathiasbynens.be/notes/async-analytics-snippet#async for more information.

--- templates/iRail/footer.php
+++ templates/iRail/footer.php
@@ -1,4 +1,3 @@
-
 <?php /* Google Analytics -- please do not change the UA-code unless hosting on your own webspace. Don't commit changes to this file. */ ?>
 <script>
  // Optimized asynchronous Analytics snippet: http://mths.be/aab
@@ -6,7 +5,6 @@
  (function(d, t) {
   var g = d.createElement(t),
       s = d.getElementsByTagName(t)[0];
-  g.async = 1;
   g.src = '//www.google-analytics.com/ga.js';
   s.parentNode.insertBefore(g, s);
  }(document, 'script'));
